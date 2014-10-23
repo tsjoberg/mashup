4e1f69f7c690e2ed23370964372942305fa17355
tsjoberg <tomgsjoberg@gmail.com>
2014-10-22 21:55:01 -0400

    First Mashup Commit
    Google Maps / MTA Bustime stops

---
+++ mashup.js
@@ -0,0 +1,34 @@
+$( function() {
+
+    map = new GMaps({
+        div: '#map-canvas',
+        lat: 40.7658914,
+        lng: -73.9614134,
+        layers: ['transit'],
+        enableNewStyle: true
+    });
+
+    map.addLayer('transit');
+
+    $.getJSON("http://bustime.mta.info/api/where/stops-for-location.json?radius=100&lat=40.7658914&lon=-73.9614134&latSpan=0.005&lonSpan=0.005&key=791cb5ed-0cd7-44af-96b0-91fa6d781095&callback=?", function(data){
+        var markers = [];
+        var items = data.data.stops;
+        console.log(items);
+        for (i=0; i < items.length; i++){
+            markers.push({
+                lat: items[i].lat,
+                lng: items[i].lon,
+                title: items[i].name,
+                infoWindow: {
+                    content: "<h1>" + items[i].name + "</h1>"
+                }
+            });
+        }
+
+        console.log(markers);
+
+        map.addMarkers(markers);
+    })
+
+
+});
